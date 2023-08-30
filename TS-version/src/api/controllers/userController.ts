1277f769fdaf0c827f6ab46d9e4cb7d3af6bbe58
IlonaJuv <ilonaju@metropolia.fi>
2023-08-30 14:45:00 +0300

    first commit

---
+++ TS-version/src/api/controllers/userController.ts
@@ -0,0 +1,193 @@
+import {
+  addUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from '../models/userModel';
+import {Request, Response, NextFunction} from 'express';
+import CustomError from '../../classes/CustomError';
+import bcrypt from 'bcryptjs';
+import {PostUser, User} from '../../interfaces/User';
+import {validationResult} from 'express-validator';
+const salt = bcrypt.genSaltSync(12);
+
+const userListGet = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await getAllUsers();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const userGet = async (
+  req: Request<{id: string}, {}, {}>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await getUser(req.params.id);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// TDOD: create userPost function to add new user
+// userPost should use addUser function from userModel
+// userPost should use validationResult to validate req.body
+// userPost should use bcrypt to hash password
+
+const userPost = async (
+  req: Request<{}, {}, PostUser>,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    next(new CustomError('Validation errors', 400));
+    return;
+  }
+
+  try {
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    const {user_name, email} = req.body;
+    const newUser: PostUser = {
+      password: hashedPassword,
+      role: 'user',
+      user_name: user_name,
+      email: email,
+    };
+    console.log(newUser);
+    const user_id = await addUser(newUser);
+    if (user_id) {
+      res.json({message: 'User added successfully', user_id});
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const userPut = async (
+  req: Request<{id: number}, {}, User>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if ((req.user as User).role !== 'admin') {
+      throw new CustomError('Admin only', 403);
+    }
+
+    const user = req.body;
+
+    const result = await updateUser(user, req.params.id);
+    if (result) {
+      res.json({
+        message: 'user modified',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+// TODO: create userPutCurrent function to update current user
+// userPutCurrent should use updateUser function from userModel
+// userPutCurrent should use validationResult to validate req.body
+
+const userPutCurrent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req.body);
+  if (!errors.isEmpty()) {
+    const messages: string = errors
+      .array()
+      .map((error) => `${error.msg}: ${error.param}`)
+      .join(', ');
+    console.log('user_delete validation', messages);
+    next(new CustomError(messages, 400));
+    return;
+  }
+  try {
+    const user = req.body;
+    const result = await updateUser(user, (req.user as User).user_id);
+    if (result) {
+      res.json({
+        message: 'user modified',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+// TODO: create userDelete function for admin to delete user by id
+// userDelete should use deleteUser function from userModel
+// userDelete should use validationResult to validate req.params.id
+// userDelete should use req.user to get role
+
+const userDelete = async (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req.params);
+  if (!errors.isEmpty()) {
+    const messages: string = errors
+      .array()
+      .map((error) => `${error.msg}: ${error.param}`)
+      .join(', ');
+    console.log('user_delete validation', messages);
+    next(new CustomError(messages, 400));
+    return;
+  }
+  try {
+    if ((req.user as User).role !== 'admin') {
+      throw new CustomError('Admin only', 403);
+    }
+
+    const result = await deleteUser(parseInt(req.params.id));
+    if (result) {
+      res.json({
+        message: 'user deleted',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const userDeleteCurrent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await deleteUser((req.user as User).user_id);
+    if (result) {
+      res.json({
+        message: 'user deleted',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const checkToken = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.user) {
+    next(new CustomError('token not valid', 403));
+  } else {
+    res.json(req.user);
+  }
+};
+
+export {
+  userListGet,
+  userGet,
+  userPost,
+  userPut,
+  userPutCurrent,
+  userDelete,
+  userDeleteCurrent,
+  checkToken,
+};
